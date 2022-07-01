eae5c16f72aa122fe75e9ff590e495c0abf06198
hallibentley <hallibentley@gmail.com>
2022-07-01 10:55:53 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,7 @@ import MainView from './components/main-view/main-view';
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
-const store = createStore(moviesApp);
+const store = createStore(moviesApp());
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
