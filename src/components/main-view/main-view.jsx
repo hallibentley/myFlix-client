dcd233d4824a8b48eb351782e6210a5b2de5d46e
hallibentley <hallibentley@gmail.com>
2022-07-05 12:15:18 -0400

    debug

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -73,11 +73,12 @@ class MainView extends React.Component {
           <Row className="main-view justify-content-md-center">
 
             <Route exact path="/" render={() => {
-              if (!user) return <Col>
-                <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
-              </Col>
-              if (movies.length === 0) return <div className="main-view" />;
-              return <MoviesList movies={movies} />;
+              console.log('hello');
+              // if (!user) return <Col>
+              //   <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+              // </Col>
+              // if (movies.length === 0) return <div className="main-view" />;
+              // return <MoviesList movies={movies} />;
             }} />
 
             <Route exact path="/register" render={() => {
