ed2238bae5e87d6b52f3314fc1574ebdaaa637be
hallibentley <hallibentley@gmail.com>
2022-07-05 12:27:19 -0400

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -73,12 +73,11 @@ class MainView extends React.Component {
           <Row className="main-view justify-content-md-center">
 
             <Route exact path="/" render={() => {
-              console.log('hello');
-              // if (!user) return <Col>
-              //   <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
-              // </Col>
-              // if (movies.length === 0) return <div className="main-view" />;
-              // return <MoviesList movies={movies} />;
+              if (!user) return <Col>
+                <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+              </Col>
+              if (movies.length === 0) return <div className="main-view" />;
+              return <MoviesList movies={movies} />;
             }} />
 
             <Route exact path="/register" render={() => {
